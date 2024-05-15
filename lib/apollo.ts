44abef02fe9789779370d05d13d8bdba6201a7b7
tahaamin2013 <tahaamindob2013@gmail.com>
2024-05-15 16:51:41 +0500

    add new url at apollo.ts

--- lib/apollo.ts
+++ lib/apollo.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 const apolloClient = new ApolloClient({
-  uri: 'http://localhost:3000/api/tasks',
+  uri: 'https://project-mangement-tool.vercel.app/api/tasks',
   cache: new InMemoryCache(),
 })
 
