2d1fabec40de53d16145982b42366f35d4f3ac05
tahaamin2013 <tahaamindob2013@gmail.com>
2024-05-15 16:03:58 +0500

    auth

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ import '../styles/globals.css';
 import { ApolloProvider } from '@apollo/client';
 import apolloClient from '../lib/apollo';
 import Layout from "../components/layout";
-
+import {SessionProvider} from "next-auth"
 function MyApp({ Component, pageProps }) {
   return    <ApolloProvider client={apolloClient}>
     <Layout>
